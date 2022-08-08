2876d2975a45768bf164f542fbfbd781a92e277b
Kieran Wood <kieranw098@gmail.com>
2022-08-08 01:31:47 -0600

    Added single user view page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Standard library dependencies
 from typing import List
+from random import randint, choice
 
 # Internal Dependencies
 from models import User
@@ -16,19 +17,17 @@ app = FastAPI(title="Descent098 FastAPI Testing",
     debug=True
     )
 
+users = [User()]
+for index, _ in enumerate(range(randint(2,55))):
+    users.append(User(name=choice(["Kieran", "Craig", "Roberio", "Liao"]), age=randint(18,75), country=choice(["Canada", "USA", "Brazil", "Taiwan"]), user_id=2+index))
 
-
-users= [ # Some test data to work with
-    User(),
-    User(name="john", age=22, country="Canada", user_id = 2)
-]
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/users/{user_id}", response_model=User, name="Get user by ID")
+@app.get("/user/{user_id}", response_model=User, name="Get user by ID")
 async def get_user(user_id:int):
     """Gets users based on provided ID
 
@@ -39,6 +38,19 @@ async def get_user(user_id:int):
     """
     return vars(users[user_id-1])
 
+
+@app.get("/fancy/user/{user_id}", response_class=HTMLResponse, name="Get user by ID with a nice frontend")
+async def get_user_fancy(user_id:int, request:Request):
+    """Get user by ID with a nice frontend
+
+    Parameters
+    ----------
+    user_id : int
+        The id of the user to get
+    """
+    gender = choice(["male", "female"])
+    return templates.TemplateResponse("single_user.html", {"request": request,"app":app, "user":users[user_id-1], "gender":gender})
+
 @app.get("/users", response_model=List[User], name="Get All users")
 async def get_users():
     """Gets all users"""
@@ -46,8 +58,8 @@ async def get_users():
 
 @app.get("/user", response_class=RedirectResponse)
 async def get_user_incorrect():
-    """If someone forgets an ID they will be redirected to /users/1"""
-    return RedirectResponse("/users/1")
+    """If someone forgets an ID they will be redirected to /"""
+    return RedirectResponse("/")
 
 @app.post("/user")
 async def create_user(name:str = Form(example="kieran"), age:int = Form(example=23), country:str = Form(example="Canada")):
